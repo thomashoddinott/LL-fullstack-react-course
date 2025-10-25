90e9e107f7b6b33fac82ae1972543ea0d2a3afe4
Thomas Hoddinott <thomashoddinott477@gmail.com>
2025-10-25 20:31:21 +0200

    Upvote articles with temp in-memory storage

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,18 +1,24 @@
 import express from "express";
 
+const articleInfo = [
+  { name: "learn-node", upvotes: 0 },
+  { name: "learn-react", upvotes: 0 },
+  { name: "mongodb", upvotes: 0 },
+];
+
 const app = express();
 app.use(express.json());
 
-app.get("/hello", function (req, res) {
-  res.send("Hello!");
-});
-
-app.get("/hello/:name", function (req, res) {
-  res.send("Hello, " + req.params.name);
-});
-
-app.post("/hello", function (req, res) {
-  res.send("Hello, " + req.body.name + "!");
+app.post("/api/articles/:name/upvote", (req, res) => {
+  const article = articleInfo.find((a) => a.name === req.params.name);
+  article.upvotes += 1;
+  res.send(
+    "Success! The article " +
+      req.params.name +
+      " now has " +
+      article.upvotes +
+      " upvotes!"
+  );
 });
 
 app.listen(8000, function () {
