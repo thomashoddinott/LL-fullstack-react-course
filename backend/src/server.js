177158c3774a9efccb66329ed14409523af2eae4
Thomas Hoddinott <thomashoddinott477@gmail.com>
2025-10-25 20:17:15 +0200

    add a basic POST endpoint

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,10 +2,16 @@ import express from "express";
 
 const app = express();
 
+app.use(express.json());
+
 app.get("/hello", function (req, res) {
   res.send("Hello!");
 });
 
-app.listen(8000, function() {
-    console.log('Server is listening on port 8000')
-})
+app.post("/hello", function (req, res) {
+  res.send("Hello, " + req.body.name + "!");
+});
+
+app.listen(8000, function () {
+  console.log("Server is listening on port 8000");
+});
