65419a465b09b139ea36bcd4441e35e12e712c54
Thomas Hoddinott <thomashoddinott477@gmail.com>
2025-10-25 20:24:10 +0200

    Using route parameters

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,14 @@
 import express from "express";
 
 const app = express();
-
 app.use(express.json());
 
 app.get("/hello", function (req, res) {
   res.send("Hello!");
+});
+
+app.get("/hello/:name", function (req, res) {
+  res.send("Hello, " + req.params.name);
 });
 
 app.post("/hello", function (req, res) {
