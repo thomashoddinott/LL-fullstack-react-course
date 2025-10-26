520349be3962e5d38615186b488d72fed56b3f4d
Thomas Hoddinott <thomashoddinott477@gmail.com>
2025-10-26 15:18:25 +0100

    serve FE from BE

--- backend/readme.md
+++ backend/readme.md
@@ -3,3 +3,5 @@ Go to https://cloud.mongodb.com/ to get uri for `.env`
 
 # Firebase
 Generate new private key > https://console.firebase.google.com/u/0/project/ll-fullstack-react/settings/serviceaccounts/adminsdk
+
+# Build FE with `npm run build` and move to backend. TODO: automate
--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,11 @@ import express from "express";
 import { MongoClient, ServerApiVersion } from "mongodb";
 import admin from "firebase-admin";
 import fs from "fs";
+import path from "path";
+
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const credentials = JSON.parse(fs.readFileSync("./firebase-credentials.json"));
 admin.initializeApp({
@@ -29,6 +34,12 @@ async function connectToDb() {
 
   db = client.db("fullstack-react-db");
 }
+
+app.use(express.static(path.join(__dirname, "../dist")));
+
+app.get(/^(?!\/api).+/, (req, res) => {
+  res.sendFile(path.join(__dirname, "../dist/index.html"));
+});
 
 app.get("/api/articles/:name", async (req, res) => {
   const { name } = req.params;
@@ -88,9 +99,11 @@ app.post("/api/articles/:name/comments", async (req, res) => {
   res.json(updatedArticle);
 });
 
+const PORT = process.env.PORT || 8000
+
 async function start() {
   await connectToDb();
-  app.listen(8000, () => console.log("Server is listening on port 8000"));
+  app.listen(PORT, () => console.log("Server is listening on port " + PORT));
 }
 
 start();
