7f1891d8a2a42d942eace024efce69355cfb207d
Thomas Hoddinott <thomashoddinott477@gmail.com>
2025-10-26 14:36:47 +0100

    add middleware to load user

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,7 +4,7 @@ import { MongoClient, ServerApiVersion } from "mongodb";
 import admin from "firebase-admin";
 import fs from "fs";
 
-const credentials = JSON.parse(fs.readFileSync('./firebase-credentials.json'));
+const credentials = JSON.parse(fs.readFileSync("./firebase-credentials.json"));
 admin.initializeApp({
   credential: admin.credential.cert(credentials),
 });
@@ -32,10 +32,21 @@ async function connectToDb() {
 
 app.get("/api/articles/:name", async (req, res) => {
   const { name } = req.params;
-
   const article = await db.collection("articles").findOne({ name });
-
   res.json(article);
+});
+
+app.use(async function (req, res, next) {
+  const { authtoken } = req.headers;
+
+  if (authtoken) {
+    const user = await admin.auth().verifyIdToken(authtoken);
+    req.user = user;
+  } else {
+    res.sendStatus(400);
+  }
+
+  next();
 });
 
 app.post("/api/articles/:name/upvote", async (req, res) => {
