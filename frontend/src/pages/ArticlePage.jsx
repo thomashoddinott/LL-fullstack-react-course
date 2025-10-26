fe129af05bcd172c3887d788bb11a737f674e70f
Thomas Hoddinott <thomashoddinott477@gmail.com>
2025-10-26 09:48:02 +0100

    implement upvote button with `useState`

--- frontend/src/pages/ArticlePage.jsx
+++ frontend/src/pages/ArticlePage.jsx
@@ -3,16 +3,25 @@ import { useLoaderData } from "react-router";
 import articles from "../article-content";
 import axios from "axios";
 import CommentsList from "../CommentsList";
+import { useState } from "react";
 
 export default function ArticlesPage() {
   const { name } = useParams();
-  const { upvotes, comments } = useLoaderData();
+  const { upvotes: initialUpvotes, comments } = useLoaderData();
+  const [upvotes, setUpvotes] = useState(initialUpvotes);
 
   const article = articles.find((a) => a.name === name);
+
+  async function onUpvoteClicked() {
+    const response = await axios.post("/api/articles/" + name + "/upvote");
+    const updatedArticleData = response.data;
+    setUpvotes(updatedArticleData.upvotes);
+  }
 
   return (
     <>
       <h1>{article.title}</h1>
+      <button onClick={onUpvoteClicked}>Upvote</button>
       <p>This article has {upvotes} upvotes!</p>
       {article.content.map((p) => (
         <p key={p}>{p}</p>
