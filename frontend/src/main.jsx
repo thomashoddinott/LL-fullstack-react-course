06ba66a82ca4b7433ec213e89f2054478c33e4d4
Thomas Hoddinott <thomashoddinott477@gmail.com>
2025-11-04 14:33:56 +0100

    not a security risk apparently (this commit was originally flagged by github) - see https://stackoverflow.com/questions/37482366/is-it-safe-to-expose-firebase-apikey-to-the-public

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -8,15 +8,13 @@ import { initializeApp } from "firebase/app";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
-console.log(import.meta.env.VITE_FIREBASE_API_KEY)
-
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  apiKey: "AIzaSyA9pUBrGUDb77r0LuJ-BLweoTAN6f8e5RA",
+  authDomain: "ll-fullstack-react.firebaseapp.com",
+  projectId: "ll-fullstack-react",
+  storageBucket: "ll-fullstack-react.firebasestorage.app",
+  messagingSenderId: "910886681467",
+  appId: "1:910886681467:web:64b1f9002a6023ff395bde"
 };
 
 // Initialize Firebase
